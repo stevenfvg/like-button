240e4d4f37245a1ce7dd68e45062221e282c009d
stevenfvg <stevenvalladaresgiron@gmail.com>
2024-03-02 00:48:24 -0400

    refactor: :recycle: counter refactoring

--- counter.js
+++ counter.js
@@ -2,7 +2,13 @@ export function setupCounter(element) {
     let counter = 0;
     const setCounter = count => {
         counter = count;
-        element.innerHTML = `Me gusta <i class="bi bi-hand-thumbs-up-fill"></i> ${counter}`;
+        //element.innerHTML = `Me gusta <i class="bi bi-hand-thumbs-up-fill"></i> ${counter}`;
+        if (counter >= 1) {
+            element.innerHTML = `Me gusta <i class="bi bi-hand-thumbs-up-fill"></i> ${counter}`;
+            console.log('Total likes: '.concat(counter));
+        } else {
+            element.innerHTML = `Me gusta <i class="bi bi-hand-thumbs-up-fill"></i>`;
+        }
     };
     element.addEventListener('click', () => setCounter(counter + 1));
     setCounter(0);
