d7384e950fcfde15b6d17ff56176dfab21cdc40b
stevenfvg <stevenvalladaresgiron@gmail.com>
2024-03-03 13:34:45 -0400

    refactor: :recycle: refactoring of main.js file

--- counter.js
+++ counter.js
@@ -1,15 +1,19 @@
-export function setupCounter(element) {
+export function setupCounter(element1, element2) {
     let counter = 0;
+
     const setCounter = count => {
         counter = count;
-        //element.innerHTML = `Me gusta <i class="bi bi-hand-thumbs-up-fill"></i> ${counter}`;
         if (counter >= 1) {
-            element.innerHTML = `Me gusta <i class="bi bi-hand-thumbs-up-fill"></i> ${counter}`;
+            element1.innerHTML = `Me gusta <i class="bi bi-hand-thumbs-up-fill"></i> ${counter}`;
             console.log('Total likes: '.concat(counter));
+            element2.style.display = 'inline';
         } else {
-            element.innerHTML = `Me gusta <i class="bi bi-hand-thumbs-up-fill"></i>`;
+            element1.innerHTML = `Me gusta <i class="bi bi-hand-thumbs-up-fill"></i>`;
+            element2.style.display = 'none';
         }
     };
-    element.addEventListener('click', () => setCounter(counter + 1));
+    element1.addEventListener('click', () => setCounter(counter + 1));
+    element2.addEventListener('click', () => setCounter(counter - 1));
     setCounter(0);
+
 }
--- main.js
+++ main.js
@@ -5,8 +5,12 @@ document.querySelector('#app').innerHTML = `
   <div>
     <div class="card">
       <button id="counter" type="button"></button>
+      <button id="decrement" type="button"><i class="bi bi-hand-thumbs-down-fill"></i></button>
     </div>
   </div>
 `;
 
-setupCounter(document.querySelector('#counter'));
+setupCounter(
+    document.querySelector('#counter'),
+    document.querySelector('#decrement')
+);
